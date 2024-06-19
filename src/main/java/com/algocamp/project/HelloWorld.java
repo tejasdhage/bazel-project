4fa88f425c0c331a312da91abc3f2c7b5796d1c5
Tejas Dhage <tejasdhage@Tejass-MacBook-Air.local>
2024-06-19 08:21:10 +0530

    first commit

---
+++ src/main/java/com/algocamp/project/HelloWorld.java
@@ -0,0 +1,10 @@
+package main.java.com.algocamp.project;
+
+import main.java.com.algocamp.project.utils.MyUtils;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hi there");
+        MyUtils.printString();
+    }
+}
---
+++ src/main/java/com/algocamp/project/utils/MyUtils.java
@@ -0,0 +1,7 @@
+package main.java.com.algocamp.project.utils;
+
+public class MyUtils {
+    public static void printString() {
+        System.out.println("My String");
+    }
+}
